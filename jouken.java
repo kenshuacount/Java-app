fe1e53a7fb80952f6746aae912a9e37aef890491
kenshuacount <takahashi.rikiya1025@gmail.com>
2014-09-15 17:16:13 +0900

    jouken

---
+++ jouken.java
@@ -0,0 +1,10 @@
+public class jouken{
+	public static void main(String[] args){
+		int age = 12;
+		if(age > 20){
+			System.out.println("大人です！");
+		}else{
+			System.out.println("子供です！");
+		}
+	}
+}
