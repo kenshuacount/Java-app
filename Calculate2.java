6bc1beb1b92fd6aefb4ff3a879c10b9a32993614
kenshuacount <takahashi.rikiya1025@gmail.com>
2014-09-15 16:57:50 +0900

    keisan2

---
+++ Calculate2.java
@@ -0,0 +1,21 @@
+public class Calculate2
+{
+	public static void main(String[] args){
+
+	int addResult;
+	int minResult;
+	int mulResult;
+	int divResult;
+
+	addResult = 6 + 3;
+	minResult = 6 - 3;
+	mulResult = 6 * 3;
+	divResult = 6 / 3;
+
+	System.out.println(addResult);
+	System.out.println(minResult);
+	System.out.println(mulResult);
+	System.out.println(divResult);
+
+	}
+}
